c3ea586fe92b1d33fd29d4b6921e3da5a2cff27d
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2025-11-29 14:43:40 +0530

    create model for task file

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Task(models):
+    DEPENDENCIES = {}
+    title = models.CharField(max_length = 200)
+    due_date = models.DateField()
+    estimated_hours = models.IntegerField()
+    importance = models.IntegerField(
+        validators=[
+            MinValueValidator(1, message="Value must be at least 1."),
+            MaxValueValidator(10, message="Value cannot exceed 10.")
+        ]
+    )
+    dependencies = models.CharField(max_length=1,choices=DEPENDENCIES)
